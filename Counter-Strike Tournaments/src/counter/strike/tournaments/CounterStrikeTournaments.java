496c0629454960fdea2644f7571fbe3cd4b5362c
Stewil <32218463+Stewil@users.noreply.github.com>
2018-03-11 11:29:36 +0100

    Added part SQL implementation in main method

--- Counter-Strike Tournaments/src/counter/strike/tournaments/CounterStrikeTournaments.java
+++ Counter-Strike Tournaments/src/counter/strike/tournaments/CounterStrikeTournaments.java
@@ -1,12 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package counter.strike.tournaments;
 
+import org.postgresql.Driver; 
+import java.sql.*;
+
 /**
- *
  * @author 103020
  */
 public class CounterStrikeTournaments implements Main_Interface{
@@ -15,7 +12,32 @@ public class CounterStrikeTournaments implements Main_Interface{
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // Address, username, and password for the SQL database.
+        String url = "";
+        String username = "";
+        String password = "";
+        
+        try {
+            /* Establish a connection with the database, using the address,
+            ** username, and password. 
+            */
+            Connection db = DriverManager.getConnection(url, username, password);
+            
+            /* Object used for executing a static SQL statement, and returning 
+            ** the results. This object can hold one ResultSet at a time. If 
+            ** more are needed, more statement objects need to be created.
+            */
+            Statement st = db.createStatement();
+            
+            /* Object used to get a result from a database. the string needs to
+            ** be the SQL code for acquiring the desired set.
+            */            
+            ResultSet rs = st.executeQuery("");
+            
+        } catch (Exception e) {
+            // print out the exception, for debugging.
+            System.out.println(e);
+        }
     }
 
     @Override
