e9038138a8d08cfcac78da76eaade515e549f078
Stewil <32218463+Stewil@users.noreply.github.com>
2018-03-11 15:00:09 +0100

    Added further functionality for sending SQL queries

--- Counter-Strike Tournaments/src/counter/strike/tournaments/CounterStrikeTournaments.java
+++ Counter-Strike Tournaments/src/counter/strike/tournaments/CounterStrikeTournaments.java
@@ -8,7 +8,9 @@ import java.util.Scanner;
  * @author 103020
  */
 public class CounterStrikeTournaments implements Main_Interface{
-
+    static Connection db;
+    static Statement st;
+    static ResultSet rs;
     /**
      * @param args the command line arguments
      */
@@ -22,19 +24,22 @@ public class CounterStrikeTournaments implements Main_Interface{
             /* Establish a connection with the database, using the address,
             ** username, and password. 
             */
-            Connection db = DriverManager.getConnection(url, username, password);
+            db = DriverManager.getConnection(url, username, password);
             
             /* Object used for executing a static SQL statement, and returning 
             ** the results. This object can hold one ResultSet at a time. If 
             ** more are needed, more statement objects need to be created.
             */
-            Statement st = db.createStatement();
+            st = db.createStatement();
             
             /* Object used to get a result from a database. the string needs to
             ** be the SQL code for acquiring the desired set. "maintains a 
             ** cursor pointing to its current row of data."
+            ** Is instantiated to default by showing all entries in People
             */            
-            ResultSet rs = st.executeQuery("SELECT * FROM PEOPLE");
+            rs = st.executeQuery("SELECT * FROM PEOPLE");
+            
+            Send(Input());
             
             Display(rs);
             // Closing ResultSet and Statement
@@ -52,14 +57,14 @@ public class CounterStrikeTournaments implements Main_Interface{
     ** int, and verifies that it's correct, else it asks user to try again.
     ** Returns an int based on the choice made.
     */
-    static int input(){
+    static int Input(){
         int j = 0;
         Boolean bool = true;
         Scanner kb = new Scanner(System.in);
         while (bool) {
             System.out.println("Enter the number for the option you wish to choose:");
             System.out.println("All coaches, and the team the coach is on: 1");
-            System.out.println("All people in a specific team: 2");
+            System.out.println("All people on a teams that has won at least one tournament: 2");
             System.out.println("All names of teams, and the number of players on each team: 3");
             System.out.println("All teams with more wins than input number: 4");
             j = kb.nextInt();
@@ -71,9 +76,36 @@ public class CounterStrikeTournaments implements Main_Interface{
         }
         return j;
     }
- 
-    public static void Send(String s) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    
+    /**
+     * Method for sending the desired SQL query, based on the input number from
+     * Input() method.
+     * @param j the input number from the Input() method.
+     */
+
+    public static void Send(int j) {
+        try {
+        switch (j){
+            // All coaches, and the team the coach is on
+            case 1: rs = st.executeQuery("SELECT * FROM PEOPLE INNER JOIN IN_TEAM ON PEOPLE.EMAIL=IN_TEAM.EMAIL" +
+                        "WHERE PEOPLE.EMAIL IN (SELECT TEAMS.COACH FROM TEAMS)");
+                break;
+            // All people on a teams that has won at least one tournament
+            case 2: rs = st.executeQuery("SELECT * FROM PEOPLE INNER JOIN " +
+                    "IN_TEAM ON PEOPLE.EMAIL=IN_TEAM.EMAIL WHERE PEOPLE.EMAIL" +
+                    " IN (SELECT IN_TEAM.EMAIL FROM IN_TEAM WHERE " +
+                    "IN_TEAM.T_NAME IN (SELECT TOURNAMENTS.WINNER FROM TOURNAMENTS))");
+                break;
+            // All names of teams, and the number of players on each team
+            case 3: rs = st.executeQuery("SELECT TEAMS.NAME, TEAMS.PLAYERS FROM TEAMS");
+                break;
+            //case 4: 123;
+            //    break;
+            default: System.out.println("Invalid input!");
+        }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
     }
 
     /**
